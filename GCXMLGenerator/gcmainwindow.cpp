4918915f663b4b74a97bdf37e1b1b147717d919c
goblincoding <devnull@localhost>
2012-11-21 12:33:45 +0200

    Added license terms.

--- GCXMLGenerator/gcmainwindow.cpp
+++ GCXMLGenerator/gcmainwindow.cpp
@@ -1,3 +1,32 @@
+/* Copyright (c) 2012 by William Hallatt.
+ *
+ * This file forms part of "GoblinCoding's XML Studio".
+ *
+ * The official website for this project is <http://www.goblincoding.com> and,
+ * although not compulsory, it would be appreciated if all works of whatever
+ * nature using this source code (in whole or in part) include a reference to
+ * this site.
+ *
+ * Should you wish to contact me for whatever reason, please do so via:
+ *
+ *                 <http://www.goblincoding.com/contact>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (GNUGPL.txt).  If not, see
+ *
+ *                    <http://www.gnu.org/licenses/>
+ */
+
 #include "gcmainwindow.h"
 #include "ui_gcmainwindow.h"
 #include "db/gcdatabaseinterface.h"
@@ -158,7 +187,7 @@ GCMainWindow::GCMainWindow( QWidget *parent ) :
   }
 
   m_domDoc   = new QDomDocument;
-  m_settings = new QSettings( "GoblinCoding", "XML Studio", this );
+  m_settings = new QSettings( "William Hallatt", "GoblinCoding's XML Studio", this );
   m_settings->setValue( "Messages", "Save dialog prompt user preferences.");
 
   /* If the interface was successfully initialised, prompt the user to choose a database
