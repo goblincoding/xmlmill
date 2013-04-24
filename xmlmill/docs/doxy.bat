00b97d5ecd9995c41c8c1d6c80c8e75493d9f24f
goblincoding <devnull@localhost>
2013-04-24 07:13:37 +0200

    Fixed doxy.bat

--- xmlmill/docs/doxy.bat
+++ xmlmill/docs/doxy.bat
@@ -1,10 +1,8 @@
 @echo off
-
 START /WAIT doxygen WindowsDoxyfile
 cd latex
-START /WAIT make.bat
+CALL make.bat
 cd ..\
 copy .\latex\refman.pdf xmlmill.pdf
 copy .\html\xmlmill.chm .
-
 @echo on
--- xmlmill/docs/doxy.sh
+++ xmlmill/docs/doxy.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 doxygen UbuntuDoxyfile 
-echo "done with doxygen"
 cd latex/
-echo "starting make"
 make 
-echo "done with make"
 cd ../
 cp latex/refman.pdf ./xmlmill.pdf
 
